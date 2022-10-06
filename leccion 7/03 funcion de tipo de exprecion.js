bc7dc1540381ba2cb70fc0392bb634b0ffe51280
VicoVillca <vicovillca@hotmail.com>
2022-10-06 12:05:57 -0400

    leccion 8

--- leccion 7/03 funcion de tipo de exprecion.js
+++ leccion 7/03 funcion de tipo de exprecion.js
@@ -1,8 +1,45 @@
-let x = function(a,b){return a+b;}
+let Sumar = function(a,b){
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    console.log(arguments[2]);
+    return a+b;
+}
 
-let resultado =x(2,4);
+let resultado =Sumar(2,4);
 console.log(resultado);
 
-(function (a,b){
-    console.log("suma = "+(a+b));
-})(2,3)
+const sumarFuntionTipoFlecha = (a,b)=>{
+    console.log("Hola chiquitaaa");    
+    return a+b
+};
+
+resultado = sumarFuntionTipoFlecha(2,4);
+console.log(resultado);
+
+resultado = sumarFuntionTipoFlecha(2,7);
+console.log(resultado);
+
+
+let total =SumaTotal(2,3,4,5,6,7,8,9);
+console.log(total);
+
+function SumaTotal(){
+    let c=0;
+    if(arguments.length>0){
+        for (let i = 0; i < arguments.length; i++) {
+            c=c+arguments[i];
+        }
+    }   
+    return c;
+}
+
+const persona={
+    nombre:"juan",
+    apellido:"Mmani"
+}
+function modificamosNombre(p1){
+    p1.nombre="Maria";
+}
+modificamosNombre(persona);
+
+console.log(persona);
