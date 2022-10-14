376ce71ee7417bc8ccf8326e32743c870f72db85
VicoVillca <vicovillca@hotmail.com>
2022-10-13 23:52:40 -0400

    funciones en objetos

--- leccion 8/04 constructor de objetos.js
+++ leccion 8/04 constructor de objetos.js
@@ -3,6 +3,9 @@ function Persona(nombre,apellido,email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre+" "+this.apellido;
+    }
 }
 
 let persona = new Persona("Juan", "Mamani","juan@gmail.com");
@@ -10,5 +13,11 @@ let persona = new Persona("Juan", "Mamani","juan@gmail.com");
 console.log(persona);
 let persona2 =  new Persona();
 persona2.nombre = "Maria";
+persona2.apellido = "Magali";
 
 console.log(persona2.nombre);
+
+
+console.log(persona.nombreCompleto());
+
+console.log(persona2.nombreCompleto());
